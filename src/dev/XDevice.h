48990d63b642c3c7561338e047dd65b707676b42
t.j <thomas.jonitz@gmail.com>
2013-10-01 23:01:32 +0200

    daily commit. changed to newer version of comment style and added documentation on class 'src/dev/XDevice

--- src/dev/XDevice.h
+++ src/dev/XDevice.h
@@ -1,7 +1,7 @@
 /**
- * @file XDevice.h
- * @author t.j.
- * @version 2013-09
+ * \file XDevice.h
+ * \author t.j.
+ * \version 2013-10
  */
 #ifndef XDEVICE_H
 #define XDEVICE_H
@@ -15,8 +15,8 @@
 //#include "AbstractEncoder.h"
 
 /**
- * @brief class XDevice as Testing-Object.
- * @todo Instanciation via Factory?
+ * \brief class XDevice as Testing-Object.
+ * \todo Instanciation via Factory?
  *
  * This class is for testing purposes only. It encapsulates a Character-Device
  * with an encoder for direct access of the devices numeric value.
@@ -28,18 +28,60 @@ class XDevice : public CharacterDevice,
 {
 public:
     // INSTANCE HANDLING //
+    /**
+     * \brief   Default-Constructor.
+     * \param id
+     * \param loc
+     * \param u
+     * \param o
+     * \param f
+     */
     XDevice( std::string id, std::string loc, std::string u, double o, double f );
+    /**
+     * \brief   Constructor with lesser params.
+     * \param id
+     * \param loc
+     * \param u
+     */
     XDevice( std::string id, std::string loc, std::string u );
+    /**
+     * \brief   Copy-Constructor.
+     * \param c
+     */
     XDevice( const XDevice &c );
+    /**
+     * \brief Destructor.
+     */
     virtual ~XDevice();
+    /**
+     * \brief   Assignment-Operator
+     * \param c
+     */
     XDevice& operator=( XDevice const& c );
     
     // MEMBER HANDLING //
+    /**
+     * \brief   Getter for member 'unit'.
+     * \return  State/value of member 'unit'
+     */
     std::string get_unit() const;
+    
+    /**
+     * \brief   Setter for member 'unit'.
+     * \param u     State/value for member 'unit'
+     */
     void set_unit( std::string u );
 
     // RELEVANT STUFF //
+    /**
+     * \brief   Read out XDevice.
+     * \return  Current value read from XDevice.
+     */
     double get_value();
+    /**
+     * \brief   Set the value of XDevice.
+     * \param v     Value to set.
+     */
     void set_value( double v );
 
 protected:
@@ -47,6 +89,9 @@ protected:
     // MEMBER HANDLING //
     // RELEVANT STUFF //
 private:
+    /**
+     * \brief String representation of devices unit.
+     */
     std::string _unit;
 };
 
