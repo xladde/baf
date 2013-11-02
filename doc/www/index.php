af83f9a08d55dde86530b3613ecfe164ac559664
xladde <thomas.jonitz@gmail.com>
2013-11-02 10:23:15 +0100

    changed doc structure.

---
+++ doc/www/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title></title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+        <header></header>
+        <footer></footer>
+    </body>
+
+</html>
